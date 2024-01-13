dae1aac00115e43bfe4a428390af4f4ba1a02d67
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-01-14 00:16:44 +0100

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        
+        "about-bg": "url('/src/assets/AboutImage.svg')",
+        "hero-bg": "url('/src/assets/hero-image.png')",
+      },
+      colors: {
+        PrefooterBg: "#333333",
+        FooterBg: "#242424",
+        white: "#FFFFFF",
+        greenText: "#035057",
+        blueText: "#10367A",
+        grayText: "#031A1C",
+        orangeText: "#FE5716",
+        cardH1: "#10367A",
+        cardP: "#454C5D",
+        grayBg: "#F0ECEC", 
+      },
+    },
   },
   plugins: [],
-}
+};
