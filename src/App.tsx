d45cc7d9e610c0be2b8a7278919043332a9fd661
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-01-23 17:06:22 +0100

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import ScrollToTop from './ScrollToTop';
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Home, Services } from "./pages";
+import AboutUs from "./components/AboutUs";
+import What from "./components/Doings";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/what-we-do" element={<What />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
