da1b254fc667adc18e3b5d5f27c3db9d83aee556
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-01-27 04:09:22 +0100

    Projects Components

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Project from "../components/Projects";
+import OurProjects from "../components/OurProjects";
+
+const Projects = () => {
+  return (
+    <div>
+      <OurProjects />
+      <Project />
+    </div>
+  );
+};
+
+export default Projects;
